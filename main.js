625f8a848d162fa43ca66227f155760d343e00ea
torkilini <torkil.petersen@gmail.com>
2020-08-13 00:14:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -47,16 +47,144 @@ sendForm.addEventListener('submit', function(event) {
 
 });
 
+// Selected device object cache
+
+let deviceCache = null;
+
 
 
 // Launch Bluetooth device chooser and connect to the selected
 
 function connect() {
 
-  //
+  return (deviceCache ? Promise.resolve(deviceCache) :
+
+      requestBluetoothDevice()).
+
+      then(device => connectDeviceAndCacheCharacteristic(device)).
+
+      then(characteristic => startNotifications(characteristic)).
+
+      catch(error => log(error));
 
 }
 
+function requestBluetoothDevice() {
+
+  log('Requesting bluetooth device...');
+
+
+
+  return navigator.bluetooth.requestDevice({
+
+    filters: [{services: [0xFFE0]}],
+
+  }).
+
+      then(device => {
+
+        log('"' + device.name + '" bluetooth device selected');
+
+        deviceCache = device;
+
+
+
+        return deviceCache;
+
+      });
+  
+}
+
+
+
+// Characteristic object cache
+
+let characteristicCache = null;
+
+
+
+// Connect to the device specified, get service and characteristic
+
+function connectDeviceAndCacheCharacteristic(device) {
+
+  if (device.gatt.connected && characteristicCache) {
+
+    return Promise.resolve(characteristicCache);
+
+  }
+
+
+
+  log('Connecting to GATT server...');
+
+
+
+  return device.gatt.connect().
+
+      then(server => {
+
+        log('GATT server connected, getting service...');
+
+
+
+        return server.getPrimaryService(0xFFE0);
+
+      }).
+
+      then(service => {
+
+        log('Service found, getting characteristic...');
+
+
+
+        return service.getCharacteristic(0xFFE1);
+
+      }).
+
+      then(characteristic => {
+
+        log('Characteristic found');
+
+        characteristicCache = characteristic;
+
+
+
+        return characteristicCache;
+
+      });
+
+}
+
+
+
+// Enable the characteristic changes notification
+
+function startNotifications(characteristic) {
+
+  log('Starting notifications...');
+
+
+
+  return characteristic.startNotifications().
+
+      then(() => {
+
+        log('Notifications started');
+
+      });
+
+}
+
+
+// Output to terminal
+
+function log(data, type = '') {
+
+  terminalContainer.insertAdjacentHTML('beforeend',
+
+      '<div' + (type ? ' class="' + type + '"' : '') + '>' + data + '</div>');
+
+}
 
 
 // Disconnect from the connected device
